baf4ba133ebbef0dead9cc755dd79c9115d43167
Dylan Sound Dood <quantumdylan@gmail.com>
2019-10-04 23:43:58 -0400

    Updating to newest Unity
    Bleep bloop

---
+++ Assets/_Scripts/UI/ItemIconContainer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemIconContainer : MonoBehaviour
+{
+    public List<Sprite> icons;
+    public GameObject iconTemplate;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void drawIcons(Item[] items){
+        foreach(Item i in items){
+            Instantiate(iconTemplate);
+        }
+    }
+}
