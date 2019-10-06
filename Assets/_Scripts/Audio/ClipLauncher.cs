ace1c5287f6510dccc0cdd86b1013ebe587a0397
Dylan Sound Dood <quantumdylan@gmail.com>
2019-10-06 03:07:44 -0400

    beep boop

---
+++ Assets/_Scripts/Audio/ClipLauncher.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class ClipLauncher : MonoBehaviour
+{
+    [SerializeField] private StudioEventEmitter ev;
+    public bool launch = false;
+    public bool lastLaunch = false;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        ev = GetComponent<StudioEventEmitter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(launch){
+            ev.Play();
+            launch = false;
+        }
+    }
+
+    public void launchClip(){
+        ev.Play();
+    }
+
+    public void stopClip(){
+        ev.Stop();
+    }
+}
